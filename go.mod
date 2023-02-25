ac70e4b4611dbfa9936d342295b7772fa8c0fc87
Cédric L'homme <public@l-homme.com>
2023-02-25 00:51:44 -0500

    feat(http): add tests

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/go-co-op/gocron v1.18.0
+	github.com/stretchr/testify v1.8.1
 	go.opentelemetry.io/otel v1.13.0
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.36.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.13.0
@@ -12,25 +13,28 @@ require (
 	go.opentelemetry.io/otel/sdk v1.13.0
 	go.opentelemetry.io/otel/sdk/metric v0.36.0
 	go.opentelemetry.io/otel/trace v1.13.0
-	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb
+	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.13.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.36.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.19.0 // indirect
-	golang.org/x/net v0.4.0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.3.0 // indirect
-	golang.org/x/text v0.5.0 // indirect
-	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
-	google.golang.org/grpc v1.52.3 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20230223222841-637eb2293923 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
--- package/status/http/http_test.go
+++ package/status/http/http_test.go
@@ -1,11 +1,169 @@
 package http_test
 
-import "testing"
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
 
-// TODO tracer mock?
+	"github.com/rangzen/otel-status/package/status"
+	otelhttp "github.com/rangzen/otel-status/package/status/http"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/sdk/metric"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+)
 
 func TestHTTP_Status(t *testing.T) {
-	t.Run("a 401 error, should create a span with an error status", func(t *testing.T) {
+	t.Run("a 200 status, should create a span without an error status", func(t *testing.T) {
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+		defer mockServer.Close()
 
+		urlParsed, err := url.Parse(mockServer.URL)
+		require.NoError(t, err)
+
+		exp := tracetest.NewInMemoryExporter()
+
+		tp := sdktrace.NewTracerProvider(
+			sdktrace.WithBatcher(exp),
+		)
+
+		mockTracer := tp.Tracer("test-tracer")
+
+		rdr := metric.NewManualReader()
+		mp := metric.NewMeterProvider(metric.WithReader(rdr))
+		mockMeter := mp.Meter("test-meter")
+
+		stater := otelhttp.HTTP{
+			SC: status.Config{
+				Name:        "Test",
+				Description: "Test 200",
+				Cron:        "@99m",
+			},
+			Method: http.MethodGet,
+			URL:    urlParsed,
+			Values: nil,
+		}
+
+		err = stater.State(mockTracer, mockMeter)
+		require.NoError(t, err)
+
+		ctx := context.Background()
+		// Assert span
+		err = tp.ForceFlush(ctx)
+		require.NoError(t, err)
+
+		spans := exp.GetSpans()
+		require.Len(t, spans, 1)
+		require.Equal(t, codes.Unset, spans[0].Status.Code)
+
+		// Assert metric
+		m, err := rdr.Collect(ctx)
+		assert.NoError(t, err)
+
+		require.Len(t, m.ScopeMetrics, 1)
+		require.Len(t, m.ScopeMetrics[0].Metrics, 2)
+	})
+
+	t.Run("a 401 error, should create a span with an error status", func(t *testing.T) {
+		mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusUnauthorized)
+		}))
+		defer mockServer.Close()
+
+		urlParsed, err := url.Parse(mockServer.URL)
+		require.NoError(t, err)
+
+		exp := tracetest.NewInMemoryExporter()
+
+		tp := sdktrace.NewTracerProvider(
+			sdktrace.WithBatcher(exp),
+		)
+
+		mockTracer := tp.Tracer("test-tracer")
+
+		rdr := metric.NewManualReader()
+		mp := metric.NewMeterProvider(metric.WithReader(rdr))
+		mockMeter := mp.Meter("test-meter")
+
+		stater := otelhttp.HTTP{
+			SC: status.Config{
+				Name:        "Test",
+				Description: "Test 401",
+				Cron:        "@99m",
+			},
+			Method: http.MethodGet,
+			URL:    urlParsed,
+			Values: nil,
+		}
+
+		err = stater.State(mockTracer, mockMeter)
+		require.NoError(t, err)
+
+		// Assert span
+		ctx := context.Background()
+		err = tp.ForceFlush(ctx)
+		require.NoError(t, err)
+
+		spans := exp.GetSpans()
+		require.Len(t, spans, 1)
+		require.Equal(t, codes.Error, spans[0].Status.Code)
+
+		// Assert metric
+		m, err := rdr.Collect(ctx)
+		assert.NoError(t, err)
+
+		require.Len(t, m.ScopeMetrics, 1)
+		require.Len(t, m.ScopeMetrics[0].Metrics, 2)
+	})
+
+	t.Run("an HTTP error, should create a span with an error status", func(t *testing.T) {
+		urlParsed, err := url.Parse("https://tralalala-tsouintsouin-les-ptites-boules---aliiiaaaaaa-prt.dev")
+		require.NoError(t, err)
+
+		exp := tracetest.NewInMemoryExporter()
+		tp := sdktrace.NewTracerProvider(
+			sdktrace.WithBatcher(exp),
+		)
+		mockTracer := tp.Tracer("test-tracer")
+
+		rdr := metric.NewManualReader()
+		mp := metric.NewMeterProvider(metric.WithReader(rdr))
+		mockMeter := mp.Meter("test-meter")
+
+		stater := otelhttp.HTTP{
+			SC: status.Config{
+				Name:        "Test",
+				Description: "Test non-existent domain",
+				Cron:        "@99m",
+			},
+			Method: http.MethodGet,
+			URL:    urlParsed,
+			Values: nil,
+		}
+
+		err = stater.State(mockTracer, mockMeter)
+		require.Error(t, err)
+
+		ctx := context.Background()
+		// Assert span
+		err = tp.ForceFlush(ctx)
+		require.NoError(t, err)
+
+		spans := exp.GetSpans()
+		require.Len(t, spans, 1)
+		require.Equal(t, codes.Error, spans[0].Status.Code)
+
+		// Assert metric
+		m, err := rdr.Collect(ctx)
+		assert.NoError(t, err)
+
+		require.Len(t, m.ScopeMetrics, 1)
+		require.Len(t, m.ScopeMetrics[0].Metrics, 1)
 	})
 }
